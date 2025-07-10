0debd46115c0a01879730c356de29895b2b017e7
Jason Summers <jwsummers.dev@gmail.com>
2025-07-10 14:06:22 -0400

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,36 @@
-import type React from "react"
-import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "@/components/theme-provider"
-import "./globals.css"
+import type React from 'react';
+import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/theme-provider';
+import './globals.css';
 
 export const metadata = {
-  title: "TechMetrix - Repair Order Tracking",
-  description: "Track repair orders, monitor labor hours, and analyze performance metrics",
-    generator: 'v0.dev'
-}
+  title: 'TechTracktion - Enhance Your Efficiency',
+  description:
+    'Track repair orders, monitor labor hours, and analyze performance metrics',
+  generator: 'v0.dev',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <head>
+        <link rel='icon' href='/logo-small-crop.png' sizes='any' />
+      </head>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           <Toaster />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
